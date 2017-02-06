114c6787087fb7c15a66cc1568d0fb0b9b9e4f1b
Tongil Kim <xhddlf8070@gmail.com>
2017-02-06 11:05:10 +0000

    tongil crawling

---
+++ parse.rb
@@ -0,0 +1,16 @@
+require 'nokogiri'
+
+1.step(91, 10) do |i|
+  doc = Nokogiri::HTML(open("result_#{i}.html"))
+  doc.xpath("//tr").each do |y|
+   
+    url= "https://portal.koreatech.ac.kr"
+   # puts "#{url}#{y.attr("data-url")}" if
+     #puts y.attr("data-url")
+     
+      uri= "#{url}"+"#{y.attr("data-url")}" 
+      if "#{uri}".include?("ctt")
+      puts  "#{uri}"
+    end
+  end
+end
