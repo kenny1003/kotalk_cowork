dc9520036bde2067040a82b8cee157214c6a5b0e
Tongil Kim <xhddlf8070@gmail.com>
2017-03-13 15:23:25 +0000

    notice

--- app/controllers/notice_controller.rb
+++ app/controllers/notice_controller.rb
@@ -1,70 +1,71 @@
 class NoticeController < ApplicationController
-def destroy_notice_db
+  
+  def destroy_notice_db
    Notice.delete_all
-end
+  end
 
 #---------------------------------------
-def crawling_notice
+  def crawling_notice
     require 'nokogiri'
     require 'open-uri'
-1.step(10, 1) do |i|
-   a=1
-  doc = Nokogiri::HTML(open("https://portal.koreatech.ac.kr/ctt/bb/bulletin?b=14&ls=20&ln=#{i}&dm=m"))
-  doc.xpath("//tr").each do |y|
+    1.step(10, 1) do |i|
+      a=1
+      doc = Nokogiri::HTML(open("https://portal.koreatech.ac.kr/ctt/bb/bulletin?b=14&ls=20&ln=#{i}&dm=m"))
+      doc.xpath("//tr").each do |y|
     
-  doc.css('td')[a].text
-    url= "https://portal.koreatech.ac.kr"
+        doc.css('td')[a].text
+        url= "https://portal.koreatech.ac.kr"
    
-      uri= "#{url}"+"#{y.attr("data-url")}" 
-      if "#{uri}".include?("ctt")
+        uri= "#{url}"+"#{y.attr("data-url")}" 
+        if "#{uri}".include?("ctt")
       
-     post_notice=Notice.new
-     post_notice.url="#{url}"+"#{y.attr("data-url")}" 
-     post_notice.post_num=doc.css('td')[a-1].text
-     post_notice.title= doc.css('td')[a].text
-     post_notice.post_time=doc.css('td')[a+1].text
-     post_notice.writer=doc.css('td')[a+2].text
-     
-     post_notice.save
-     a=a+5
+           post_notice=Notice.new
+           post_notice.url="#{url}"+"#{y.attr("data-url")}" 
+           post_notice.post_num=doc.css('td')[a-1].text
+           post_notice.title= doc.css('td')[a].text
+           post_notice.post_time=doc.css('td')[a+1].text
+           post_notice.writer=doc.css('td')[a+2].text
+           
+           post_notice.save
+           a=a+5
+        end
+      end
     end
   end
-end
-
-
-end
 #-------------------------------
 
 #-------------------------------
-        def notice
-            if user_signed_in?
-                if current_user.admin_check
-                    if !Notice.exists? or Notice.last.created_at.to_i < (Time.now - 120.minutes).to_i
-                         destroy_notice_db
-                        crawling_notice
-                        notice
-    
-                    else
-                        @every_url=Notice.all
-                
-                    end
-               
-                
-                else
-                        @every_url=Notice.all
-                    if !Notice.exists?
-                        @every_url="DB에 저장된 공지사항이 없습니다. 관리자에게 문의하세요"
-                        
-                    end
-                end
-            else 
-                    @every_url=Notice.all
-                if !Notice.exists?
-                    @every_url="DB에 저장된 공지사항이 없습니다. 관리자에게 문의하세요"
-                    
-                end
+  def notice
+      if user_signed_in?
+          if current_user.admin_check
+              if !Notice.exists? or Notice.last.created_at.to_i < (Time.now - 120.minutes).to_i
+                  destroy_notice_db
+                  crawling_notice
+                  notice
 
-            end
-         end
+              else
+                  @every_url=Notice.all
+          
+              end
+              
+            
+         
+          
+          else
+                  @every_url=Notice.all
+              if !Notice.exists?
+                  @every_url="DB에 저장된 공지사항이 없습니다. 관리자에게 문의하세요"
+                  
+              end
+          end
+      else 
+              @every_url=Notice.all
+          if !Notice.exists?
+              @every_url="DB에 저장된 공지사항이 없습니다. 관리자에게 문의하세요"
+              
+          end
+
+      end
+   end
 end
 
